fa4571f533351a85088142cd9f1ed51f527db6d1
TrueXrtnn <123123058+TrueXrtnn@users.noreply.github.com>
2023-03-24 04:35:35 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        // проверка работы программы 
         EmployeeBook employeeBook = new EmployeeBook();
         employeeBook.allEmployeesAndFullInformation();
         employeeBook.departmentInformation("1");
